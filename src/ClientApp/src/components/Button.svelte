9ec678814d3cef1505fe3933eb49a31aacfdc193
thejaviertc <javier.toribiocouz@gmail.com>
2024-03-03 13:38:44 +0100

    Feat: Add Button component

---
+++ src/ClientApp/src/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+	export let faIcon: IconDefinition | undefined = undefined;
+	export let link: string;
+</script>
+
+<a class="btn {$$props.class}" href={link} role="button">
+	{#if faIcon}
+		<Fa icon={faIcon} />
+	{/if}
+	<slot />
+</a>
