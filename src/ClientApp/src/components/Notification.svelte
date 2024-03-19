c304cfebbb2c941034ad3e4f968205afbb420f96
thejaviertc <javier.toribiocouz@gmail.com>
2024-03-20 00:02:54 +0100

    Feat: Make Notification show at the bottom right corner and dissapear after 5 seconds

--- src/ClientApp/src/components/Notification.svelte
+++ src/ClientApp/src/components/Notification.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
+	// @ts-expect-error Not detected by VSCode
+	import { get_current_component } from "svelte/internal";
+
 	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+	import { onMount } from "svelte";
 	import Fa from "svelte-fa";
 
 	export let type: string;
 	export let faIcon: IconDefinition;
+
+	const currentComponent = get_current_component();
+
+	onMount(() => {
+		const timeout = setTimeout(() => {
+			currentComponent.$destroy();
+		}, 5000);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	});
 </script>
 
-<div role="alert" class="alert alert-{type} my-4">
-	<Fa icon={faIcon} />
-	<slot />
+<div class="toast toast-end">
+	<div role="alert" class="alert alert-{type} my-4">
+		<Fa icon={faIcon} />
+		<slot />
+	</div>
 </div>
