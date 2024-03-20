441222c9eca65c9c7c08be228b3391d5f3bdcc08
thejaviertc <javier.toribiocouz@gmail.com>
2024-03-20 19:28:12 +0100

    Fix: Prevent not logged users entering /logout

--- src/ClientApp/src/routes/logout/+page.server.ts
+++ src/ClientApp/src/routes/logout/+page.server.ts
@@ -1,5 +1,11 @@
 import { redirect } from "@sveltejs/kit";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.user) {
+		throw redirect(302, "/");
+	}
+};
 
 export const actions: Actions = {
 	default: async ({ cookies }) => {
