0d1e6fd573b90e4006fafc5263af1e6a28df9839
thejaviertc <javier.toribiocouz@gmail.com>
2024-04-09 11:58:39 +0200

    Code Quality: Make return consistent

--- src/ClientApp/src/routes/temas/+page.server.ts
+++ src/ClientApp/src/routes/temas/+page.server.ts
@@ -1,17 +1,21 @@
 import { API_URL } from "$lib/constants";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import type { ITopic } from "$lib/ITopic";
 
 export const load: PageServerLoad = async ({ cookies, locals }) => {
-	if (locals.user) {
-		const response = await fetch(`${API_URL}/topics`, {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${cookies.get("session_id")}`,
-			},
-		});
-
-		return {
-			topics: await response.json(),
-		};
+	if (!locals.user) {
+		throw redirect(302, "/auth");
 	}
+
+	const response = await fetch(`${API_URL}/topics`, {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${cookies.get("session_id")}`,
+		},
+	});
+
+	return {
+		topics: (await response.json()) as ITopic[],
+	};
 };
