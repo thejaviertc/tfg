5963000ab999d4cdffc07694a8534a84d0d3ae8a
thejaviertc <javier.toribiocouz@gmail.com>
2024-04-21 11:33:19 +0200

    Feat: Create TextAreaForm component

---
+++ src/ClientApp/src/components/TextAreaForm.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { type IconDefinition } from "@fortawesome/free-brands-svg-icons";
+	import Fa from "svelte-fa";
+
+	export let id: string;
+	export let label: string;
+	export let faIcon: IconDefinition;
+	export let minlength: number | undefined = undefined;
+	export let maxlength: number | undefined = undefined;
+	export let isDisabled: boolean = false;
+</script>
+
+<div class="form-control">
+	<label for={id} class="label">{label}:</label>
+	<div class="label w-72">
+		<Fa icon={faIcon} class="absolute pl-5" />
+		<textarea
+			{id}
+			name={id}
+			placeholder={label}
+			{minlength}
+			{maxlength}
+			class="textarea textarea-bordered textarea-accent pl-12 min-w-80 w-full"
+			required
+			disabled={isDisabled}
+		></textarea>
+	</div>
+</div>
