bbb7f74a46ac60f0b1b14f9ffe87b89de2f9384d
thejaviertc <javier.toribiocouz@gmail.com>
2024-05-29 11:54:19 +0200

    Feat: Add Status badges when seeing a specific topic/idea

--- src/ClientApp/src/routes/ideas/[slug]/+page.svelte
+++ src/ClientApp/src/routes/ideas/[slug]/+page.svelte
@@ -4,6 +4,7 @@
 
 	import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 	import type { ActionData } from "./$types";
+	import TStatus from "$lib/TStatus";
 
 	export let data;
 	export let form: ActionData;
@@ -14,9 +15,17 @@
 <section class="min-h-screen hero px-72">
 	<div class="bg-secondary p-6 my-10 rounded-xl">
 		<h3 class="text-black mb-2">{data.idea.title}</h3>
-		<Badge class="badge-primary" faIcon={faCalendar}>
-			{new Date(data.idea.createdAt).toLocaleDateString()}
-		</Badge>
+		<div class="flex gap-2">
+			<Badge class="badge-primary" faIcon={faCalendar}>
+				{new Date(data.idea.createdAt).toLocaleDateString()}
+			</Badge>
+			<Badge
+				class={TStatus.getColor(data.idea.status)}
+				faIcon={TStatus.getFaIcon(data.idea.status)}
+			>
+				{TStatus.toText(data.idea.status)}
+			</Badge>
+		</div>
 		<h5 class="text-black mt-4">{data.idea.shortDescription}</h5>
 		<h5 class="text-black mt-4 mb-2">{data.idea.description}</h5>
 	</div>
--- src/ClientApp/src/routes/temas/[slug]/+page.svelte
+++ src/ClientApp/src/routes/temas/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	import Badge from "$components/Badge.svelte";
 	import FormNotification from "$components/FormNotification.svelte";
 
+	import TStatus from "$lib/TStatus";
 	import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 	import type { ActionData } from "./$types";
 
@@ -14,10 +15,19 @@
 <section class="min-h-screen hero px-72">
 	<div class="bg-secondary p-6 my-10 rounded-xl">
 		<h3 class="text-black mb-2">{data.topic.title}</h3>
-		<Badge class="badge-primary" faIcon={faCalendar}>
-			{new Date(data.topic.createdAt).toLocaleDateString()}
-		</Badge>
+		<div class="flex gap-2">
+			<Badge class="badge-primary" faIcon={faCalendar}>
+				{new Date(data.topic.createdAt).toLocaleDateString()}
+			</Badge>
+			<Badge
+				class={TStatus.getColor(data.topic.status)}
+				faIcon={TStatus.getFaIcon(data.topic.status)}
+			>
+				{TStatus.toText(data.topic.status)}
+			</Badge>
+		</div>
 		<h5 class="text-black mt-4">{data.topic.shortDescription}</h5>
-		<h5 class="text-black mt-4 mb-2">{data.topic.description}</h5>
+		<h5 class="text-black my-4">{data.topic.description}</h5>
+		<!-- <Button class="btn-primary" faIcon={faPaperPlane} link="/">Solicitar Tema</Button> -->
 	</div>
 </section>
